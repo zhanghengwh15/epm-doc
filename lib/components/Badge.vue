4869803157d643aab640443bb3c0a9d59f739312
zhangheng <506472220@11.com>
2018-12-29 17:54:47 +0800

    加lib

---
+++ lib/components/Badge.vue
@@ -0,0 +1,15 @@
+<template functional>  
+  <span class="badge" :class="props.type"> <slot></slot> </span>
+</template>
+
+<script>
+export default {
+  name: 'Badge',
+  props: {
+    type: {
+      type: String,
+      default: 'tip'
+    }
+  }
+};
+</script>
